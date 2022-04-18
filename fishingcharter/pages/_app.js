b2f311c52d66c6042ff54d9387752fc81fe28a67
glennanj1 <glennan20@gmail.com>
2022-04-18 11:24:01 -0400

    added theme button plus light / darkmode button

--- fishingcharter/pages/_app.js
+++ fishingcharter/pages/_app.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import createEmotionCache from '../utility/createEmotionCache';
 import lightTheme from '../styles/theme/lightTheme';
 import darkTheme from '../styles/theme/darkTheme';
+import IconButton from '@mui/material/IconButton';
 import '../styles/globals.css';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 
 const clientSideEmotionCache = createEmotionCache();
 
 const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [theme, setTheme] = useState(lightTheme);
+
+  let handleClick = () => {
+    theme === darkTheme ? setTheme(lightTheme) : setTheme(darkTheme);
+  }
 
   return (
     <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
+        <IconButton aria-label="delete" onClick={handleClick}>
+          <Brightness4Icon/>
+        </IconButton>
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
