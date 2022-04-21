40a1c44ddf39e793272c2276ec5b6a40863b50f5
glennanj1 <glennan20@gmail.com>
2022-04-20 20:00:03 -0400

    corrected business name and added weather to home screen

--- fishingcharter/pages/index.js
+++ fishingcharter/pages/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import frank1 from '../public/frank1.png'
+import OutlinedCard from '../components/card'
 
 export default function Home(props) {
   useEffect(() => {
@@ -20,7 +21,7 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          30 Line Fishing
+          30 Line Sport Fishing
         </h1>
         <div className={styles.frankWrapper}>
           <Image alt="Frank" src={frank1} className={styles.frank} />
@@ -74,15 +75,15 @@ export default function Home(props) {
         </video>
       </section>
       <section className={styles.section}>
-       <h1 className={styles.title}>
-            Locations
+        <h1 className={styles.title}>
+            
         </h1>
         <iframe className={styles.maps} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24386.336562058146!2d-74.04792776044921!3d40.180313100000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c227e1a86b3223%3A0x9a043d400dd011bd!2sBelmar%20Manutti%20Marina!5e0!3m2!1sen!2sus!4v1649356283168!5m2!1sen!2sus" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </section>
       <section className={styles.section}>
         <h1 className={styles.title}>
-              Catches
         </h1>
+              <OutlinedCard />
       </section>
 
       <footer className={styles.footer}>
@@ -91,7 +92,7 @@ export default function Home(props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          30 Line Fishing LLC{' '}
+          30 Line Sport Fishing LLC{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
