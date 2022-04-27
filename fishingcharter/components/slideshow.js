9e19efa3e5d792833a8fb84192f21c14ef2b35b0
glennanj1 <glennan20@gmail.com>
2022-04-27 09:06:49 -0400

    slideshow with demo images

---
+++ fishingcharter/components/slideshow.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Zoom } from 'react-slideshow-image';
+import Image from 'next/image'
+import 'react-slideshow-image/dist/styles.css'
+import styles from '../styles/Home.module.css'
+
+
+const Slideshow = () => {
+  const images = [
+    '/frank1.png',
+    '/frankStriperCatch.png'
+  ];
+
+  const zoomInProperties = {
+    indicators: true,
+    scale: 1.4
+  }
+  return (
+    <div>
+      <Zoom {...zoomInProperties}>
+        {images.map((each, index) => (
+          <div key={index} className={styles.slideshowContainer} style={{width: "100vw"}}>
+            <Image 
+                height={500}
+                width={500}
+                objectPosition="center"
+                src={each} 
+                alt="Carousel" 
+            />
+          </div>
+        ))}
+      </Zoom>
+    </div>
+  )
+}
+
+export default Slideshow;
