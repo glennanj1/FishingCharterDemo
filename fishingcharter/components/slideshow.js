5fe49b0569c96042408b7eab1c573578cf34c452
glennanj1 <glennan20@gmail.com>
2022-04-28 19:13:11 -0400

    updates

--- fishingcharter/components/slideshow.js
+++ fishingcharter/components/slideshow.js
@@ -8,19 +8,23 @@ import styles from '../styles/Home.module.css'
 const Slideshow = () => {
   const images = [
     '/frank1.png',
-    '/frankStriperCatch.png'
+    '/frankStriperCatch.png',
+    'https://fishingCharter.b-cdn.net/IMG_6191.JPG'
   ];
 
   const zoomInProperties = {
     indicators: true,
-    scale: 1.4
+    scale: 1.4,
   }
+
   return (
     <div>
-      <Zoom {...zoomInProperties}>
+      <Zoom {...zoomInProperties}
+      >
         {images.map((each, index) => (
           <div key={index} className={styles.slideshowContainer} style={{width: "100vw"}}>
             <Image 
+                unoptimized
                 height={500}
                 width={500}
                 objectPosition="center"
