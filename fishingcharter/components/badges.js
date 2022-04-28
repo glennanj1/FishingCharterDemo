b6926ebad7340ef6c9e838a576d6473b15f3a24c
glennanj1 <glennan20@gmail.com>
2022-04-28 19:13:02 -0400

    react slideshow implementation

---
+++ fishingcharter/components/badges.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs, { tabsClasses } from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import CleanHandsIcon from '@mui/icons-material/CleanHands';
+import PhishingIcon from '@mui/icons-material/Phishing';
+import SetMealIcon from '@mui/icons-material/SetMeal';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import ChildFriendlyIcon from '@mui/icons-material/ChildFriendly';
+import CheckIcon from '@mui/icons-material/Check';
+
+export default function ScrollableTabsButtonVisible() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        maxWidth: { xs: 320, sm: 480 },
+        bgcolor: 'background.paper',
+      }}
+    >
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+        aria-label="visible arrows tabs example"
+        sx={{
+          [`& .${tabsClasses.scrollButtons}`]: {
+            '&.Mui-disabled': { opacity: 0.3 },
+          },
+        }}
+      >
+        <Tab icon={<SummarizeIcon />} aria-label="License" label="Licensed" />
+        <Tab icon={<RestaurantIcon />} label="Keep Catch" />
+        <Tab icon={<CleanHandsIcon />} label="Clean" />
+        <Tab icon={<PhishingIcon />} label="Live Bait" />
+        <Tab icon={<SetMealIcon />} label="Filet Fish" />
+        <Tab icon={<ConstructionIcon />} label="Equipment" />
+        <Tab icon={<ChildFriendlyIcon />} label="Child Friendly" />
+        <Tab icon={<CheckIcon />} label="10+ Years Experience" />
+      </Tabs>
+    </Box>
+  );
+}
