3cb9d1ee943ccef6ae3cb2bd32543b6851744538
glennanj1 <glennan20@gmail.com>
2022-05-03 17:42:55 -0400

    twitter fix

--- fishingcharter/pages/index.js
+++ fishingcharter/pages/index.js
@@ -29,9 +29,10 @@ export default function Home() {
         <title>30 Line Fishing</title>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@glennan_dev" />
+        <meta name="twitter:site" content="@glennan_dev" />
         <meta name="twitter:title" content="30 Line Sports Fishing LLC" />
         <meta name="twitter:description" content="Once in a lifetime fishing charter" />
-        <meta name="twitter:image" content="https://fishingCharterFinal.b-cdn.net/IMG_4428.JPEG" />
+        <meta name="twitter:image" content="https://fishingCharterFinal.b-cdn.net/frankStriperCatch.png" />
         <meta name="description" content="Once in a liftime catch"/>
         <meta property="og:description" content="Once in a lifetime catch" />
         <meta property="og:title" content="30 Line Fishing" />
