dedbc741136ce0e5c9ce4c5ff08a12a161421827
glennanj1 <glennan20@gmail.com>
2022-05-03 11:43:13 -0400

    click to call update

--- fishingcharter/pages/contact.js
+++ fishingcharter/pages/contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import styles from '../styles/Contact.module.css'
 
 function Contact() {
   return (
     <>
-      <h1>
-        Phone Number: 1111111111
-      </h1>
+      <main className={styles.main}>
+        <h1>Contact</h1>
+        <h2>
+          Click to Call: <a className={styles.phone} href="tel:+1-609-560-2123">609-560-2123</a>
+        </h2>
+      </main>
     </>
   )
 }
