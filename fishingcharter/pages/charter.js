3a95271b7688faad2159d4089ab02346e3ba4972
glennanj1 <glennan20@gmail.com>
2022-05-03 11:43:24 -0400

    added some styling and tags

--- fishingcharter/pages/charter.js
+++ fishingcharter/pages/charter.js
@@ -1,11 +1,17 @@
 import React, {useEffect} from 'react'
 import BasicDatePicker from '../components/calender'
+import styles from '../styles/Charter.module.css'
 
 function Charter() {
   return (
-    <div>
-      <BasicDatePicker />
-    </div>
+    <>
+      <main className={styles.calendar}>
+        <h1>Online Booking Coming Soon:</h1>
+        <div className={styles.calendarContainer}>
+          <BasicDatePicker />
+        </div>
+      </main>
+    </>
   )
 }
 
