faca1c5a77c96ad42273647c5f2b74662a463bdb
glennanj1 <glennan20@gmail.com>
2022-05-03 17:23:19 -0400

    changed order for twitter bot

--- fishingcharter/pages/index.js
+++ fishingcharter/pages/index.js
@@ -27,16 +27,15 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>30 Line Fishing</title>
-        <meta name="google-site-verification" content="bcMzHSchzEvEj6qSYy_Pli_3qzxguM4DLsWCjr3xTGc" />        <meta property="og:image" content="https://i.giphy.com/media/TUslLn9iOua7MFxSJ5/giphy.webp" />
-        <meta name="description" content="Once in a liftime catch"/>
-        <meta property="og:description" content="Once in a lifetime catch" />
-        <meta property="og:title" content="30 Line Fishing" />
-        <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@glennan_dev" />
         <meta name="twitter:title" content="30 Line Sports Fishing LLC" />
         <meta name="twitter:description" content="Once in a lifetime fishing charter" />
         <meta name="twitter:image" content="https://fishingCharterFinal.b-cdn.net/IMG_4428" />
+        <meta name="description" content="Once in a liftime catch"/>
+        <meta property="og:description" content="Once in a lifetime catch" />
+        <meta property="og:title" content="30 Line Fishing" />
+        <meta property="og:type" content="website" />
       </Head>
 
       <main className={styles.main}>
