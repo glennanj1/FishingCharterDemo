a22c928e6d6505941e29e85c550f9fd772b8c0e8
glennanj1 <glennan20@gmail.com>
2022-05-03 11:41:26 -0400

    back button

---
+++ fishingcharter/components/back.js
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+export default function Page() {
+  const router = useRouter()
+
+  return <span style={{"position": "fixed", "top": "8px", "left": "10px"}} onClick={() => router.back()}><ArrowBackIcon/></span>
+}
