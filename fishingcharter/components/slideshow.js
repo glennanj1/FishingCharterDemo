0ac604f334c692ce1f9c9895594458fe0d11fab0
glennanj1 <glennan20@gmail.com>
2022-05-03 14:04:39 -0400

    updated slide show cdn links

--- fishingcharter/components/slideshow.js
+++ fishingcharter/components/slideshow.js
@@ -7,9 +7,29 @@ import styles from '../styles/Home.module.css'
 
 const Slideshow = () => {
   const images = [
-    '/frank1.png',
-    '/frankStriperCatch.png',
-    'https://fishingCharter.b-cdn.net/IMG_6191.JPG'
+    'https://fishingCharterFinal.b-cdn.net/IMG_4413.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4414.JPG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4415.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4416.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4417.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4418.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4419.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4420.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4421.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4422.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4423.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4424.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4425.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4426.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4427.jpeg',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4428.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4429.jpeg',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4430.jpeg',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4431.jpeg',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4432.JPEG',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4433.jpeg',
+    'https://fishingCharterFinal.b-cdn.net/IMG_4434.JPEG',
+    
   ];
 
   const zoomInProperties = {
@@ -22,11 +42,11 @@ const Slideshow = () => {
       <Zoom {...zoomInProperties}
       >
         {images.map((each, index) => (
-          <div key={index} className={styles.slideshowContainer} style={{width: "100vw"}}>
+          <div key={index} className={styles.slideshowContainer} style={{width: "100vw", height: "100vh"}}>
             <Image 
                 unoptimized
-                height={500}
-                width={500}
+                height={900}
+                width={680}
                 objectPosition="center"
                 src={each} 
                 alt="Carousel" 
