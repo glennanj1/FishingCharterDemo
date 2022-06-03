0b62ff674d5001974c55ba5a706dbe9aa3aa05db
Dom <Domalbano35@gmail.com>
2022-06-03 11:35:17 -0400

    addded clickable phone number to frank and mikes name

--- fishingcharter/pages/index.js
+++ fishingcharter/pages/index.js
@@ -49,27 +49,42 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>30 Line Sport Fishing</h1>
         <h2>Call today to book a trip</h2>
-        <h3>Captain Frank · (609)-560-2123</h3>
-        <h3>Captain Mike · (609)-613-1027</h3>
+        <h3>
+          Captain Frank ·{" "}
+          <a className={styles.phone} href="tel:+1-609-560-2123">
+            (609)-560-2123
+          </a>{" "}
+          {" · "}
+          Captain Mike ·{" "}
+          <a className={styles.phone} href="tel:+1-609-613-1027">
+            (609)-613-1027
+          </a>
+        </h3>
+        {/* <h3>
+          Captain Mike ·{" "}
+          <a className={styles.phone} href="tel:+1-609-613-1027">
+            (609)-613-1027
+          </a>
+        </h3> */}
 
-        <div className={styles.scrollBar}>
+        {/* <div className={styles.scrollBar}>
           <ScrollableTabsButtonVisible />
-        </div>
+        </div> */}
 
         <div className={styles.grid}>
           <Link href="/about">
             <a className={styles.card} id={styles.card}>
-              <h2>About &rarr;</h2>
+              <h2>About Us &rarr;</h2>
               <p>Find in-depth information about what we do</p>
             </a>
           </Link>
 
-          <Link href="/charter">
+          {/* <Link href="/charter">
             <a href="" className={styles.card}>
               <h2>Charter Times &rarr;</h2>
               <p>Take a Look at our Calendar and schedule.</p>
             </a>
-          </Link>
+          </Link> */}
 
           <Link href="/contact">
             <a href="" className={styles.card}>
@@ -144,7 +159,6 @@ export default function Home() {
         </div>
 
         <Script
-  
           async
           defer
           type="text/javascript"
@@ -152,13 +166,11 @@ export default function Home() {
         />
       </section>
 
-
-
       <section className={`${styles.section} ${styles.map}`}>
         <h1 className={styles.title}></h1>
         <iframe
           className={styles.maps}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.6305799910137!2d-74.25961258460272!3d40.461312679359956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3cb0c224682fd%3A0x8e0f8c0933a94b47!2sRaritan%20Marina!5e0!3m2!1sen!2sus!4v1651594864857!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3409.093746968268!2d-74.03167948961593!3d40.180464873700686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4de8b2f7d0829203!2zNDDCsDEwJzQ3LjIiTiA3NMKwMDEnNTEuMiJX!5e1!3m2!1sen!2sus!4v1654270087390!5m2!1sen!2sus"
           allowFullScreen={true}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
